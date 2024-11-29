1684390f8f7d111c84805927866f46b259a1d51d
hwangyungyu <vlzkcbdmdkr@naver.com>
2024-11-29 18:30:12 +0900

    Compelete Function 9 and some function restore

--- project_program/Project_ind.cpp
+++ project_program/Project_ind.cpp
@@ -2,10 +2,35 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <ctime>
+#include <cstdlib>
 
 #include <chrono>
 #include <thread>
 using namespace std;
+
+const int MAX_MENU = 10;
+
+class Employee{
+public:
+	// 나중에 수정 예정
+	string rand_name[10];
+	Employee(){
+		rand_name[0] = "김수현";
+		rand_name[1] = "이정환";
+		rand_name[2] = "박지현";
+		rand_name[3] = "황서연";
+		rand_name[4] = "김칠우";
+		rand_name[5] = "홍길동";
+		rand_name[6] = "김재현";
+		rand_name[7] = "박건우";
+		rand_name[8] = "함시우";
+		rand_name[9] = "지현아";
+	}
+	vector<string> employee_name;
+	vector<int> employee_price;
+	bool check_employee = true;
+};
 
 class Anim{
 public:
@@ -44,14 +69,15 @@ class Menu{
 	int dessert_sell; 
 	
 	bool check_dessert_make = false;
-	 
-	int totalIncome;
+	int total_income;
+
 public:
 	int make_coffee_count;
 	Menu(){
-		totalIncome = 0;
+		total_income = 0;
 		make_coffee_count = 1;
 	}
+	Employee employ;
 	
 	void make_menu();
 	void make_dessert();
@@ -149,8 +175,7 @@ void Menu::show_menu(){
 	else{
 		cout << "아직 만들어진 디저트가 없습니다." << endl; 
 	}
-	
-	
+
 }
 
 void Menu::modify_menu(){
@@ -186,9 +211,17 @@ void Menu::modify_menu(){
 	
 }
 
+// 판매 가격에 맞추어 판매 개수를 결정합니다.
+// 임의의 수요 공급 곡선을 여러가지 설정할 예정이며
+// 임의의 수요 공급 곡선에 따라서 지정됩니다.
+// 가격이 너무 싸다면 공급자의 공급이 줄어 판매가 줄어듭니다.
+// 가격이 너무 비싸다면 수요가 줄어 판매가 줄어듭니다.
+// 둘의 지점이 일치하는 곳이 가장 총 판매량이 가장 많은 지점입니다.
+// 핵심 기능으로써 여러가지 공급 곡선을 추가할 예정입니다.
 void Menu::sell_menu(){
 	int price;
 	for(int i = 0; i < menu_price.size(); i++){
+		// 균형지점을 결정하여 줍니다.
 		price = menu_price[i] / 100; 
 		int demand = 100 - price;
     	int supply = 10 + 2 * price;
@@ -196,24 +229,62 @@ void Menu::sell_menu(){
     	int insert = min(demand, supply);
     	
     	menu_sell[i] = insert;
+
+		// 판매된 최종개수를 입력합니다. (정산에서 이용됩니다.)
+		total_income = menu_price[i] * menu_sell[i];
 	}
+
+	
 }
 
 //직원 고용 토대 제작중
 void Menu::employee(){
+	srand((unsigned)time(NULL));
+
 	string yn;
 	cout << "직원 고용" << endl;
 
 	for(int i = 0; i < 3; i++){
-		cout << "직원이름 : " << endl;
-		cout << "고용비용 : " << endl;
-		cout << "메뉴 추가 개수 : " << endl;
+		// 가격과 고용비용, 이름을 랜덤으로 뽑습니다.
+		int employee_money = rand()%10000 + 1000;
+		int menu_count_plus = rand()%3 + 1;
+		int k = rand()%9;
 
+		// 저장된 직원의 이름을 랜덤으로 보여줍니다.
+		cout << "직원이름 : "  << employ.rand_name[k] << endl;
+
+		// 랜덤으로 고용비용을 소개하여 줍니다.
+		cout << "고용비용 : " << employee_money << "원" << endl;
+
+		// 메뉴 추가의 개수도 제공하여 줍니다. (추후 확률 조정 예정)
+		cout << "메뉴 추가 개수 : " << menu_count_plus << "개" << endl;
+
+		// 한번만 고용이 가능합니다.
 		cout << "해당 직원을 고용하시겠습니까?(Y/N) : ";
 		cin >> yn;
 
 		if(yn == "Y" || yn == "y" || yn == "Yes" || yn == "yes"){
 			cout << "해당직원을 고용하였습니다." << endl;
+			// 고용 시에 만들 수 있는 메뉴 개수 추가
+			// 최대 메뉴 개수를 제한하여 추가합니다.
+			if(make_coffee_count + menu_count_plus < MAX_MENU){
+				make_coffee_count += menu_count_plus;
+			}
+			else{
+				// 메뉴의 최대개수로 제한합니다.
+				cout << "최대 메뉴 개수에 도달하였습니다.(10개)" << endl;
+				make_coffee_count = MAX_MENU;
+			}
+			
+			// Employee class에 고용한 이름과 고용 비용 vector에 입력
+			employ.employee_name.push_back(employ.rand_name[k]);
+			employ.employee_price.push_back(employee_money);
+
+			// 현재 가용 메뉴 개수를 출력하여 보여줍니다.
+			cout << "현재 가용 가능한 메뉴의 개수는 " << make_coffee_count << "개 입니다." << endl;
+
+			// 직원 고용시에 해당 날짜에는 더이상 고용하지 못하도록 합니다.
+			employ.check_employee = false;
 			break;
 		}
 		else if(yn == "N" || yn == "n" || yn == "No" || yn == "no"){
@@ -235,6 +306,7 @@ public:
 	// 메뉴 클래스를 참조합니다. 
 	Menu menu;
 	Anim anim;
+	Employee employee;
 	void run();
 };
 
@@ -303,7 +375,10 @@ void Run::run(){
 			}
 		} 
 		else{
-			//밤에 진행할 활동을 선택합니다. 
+			//밤에 진행할 활동을 선택합니다.
+			//밤이 왔다면 다시 고용할 수 있도록 함수를 조정합니다. 
+			employee.check_employee = true;
+
 			while(menu_select < 6){
 				cout << "===================행동(밤)=====================" << endl; 
 				cout << "1. 메뉴 가격 수정" << endl; 
@@ -314,13 +389,17 @@ void Run::run(){
 				cout << "6. 잠자기" << endl;
 				cout << ">> ";
 				cin >> menu_select;
-				
+
 				switch(menu_select){
 					case 1:
 						menu.modify_menu(); 
 						break;
 					case 2:
-						menu.employee();
+					// 직원 고용이 한번 이루어 졌다면 그날은 접근하지 못하도록 합니다.
+				 		if(employee.check_employee){
+							menu.employee();
+						}
+						
 						break;
 					case 3:
 						menu.show_menu();
